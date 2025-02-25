95366dbb6695965bf2fa92b365824b731587da41
degeneratorL <3248666490@qq.com>
2025-02-24 19:19:46 -0500

    opt v3

--- beras/optimizers.py
+++ beras/optimizers.py
@@ -19,25 +19,16 @@ class RMSProp:
         beta=0.9
         epsilon=1e-7
     """
-    def __init__(self, learning_rate=0.01, beta=0.9, epsilon=1e-7):
+    def __init__(self, learning_rate, beta=0.9, epsilon=1e-6):
         self.learning_rate = learning_rate
         self.beta = beta
         self.epsilon = epsilon
         self.v = defaultdict(lambda: None)  # 存储二阶梯度的指数平滑
 
-    def apply_gradients(self, trainable_params, grads):
-        for param, grad in zip(trainable_params, grads):
-            key = id(param)
-            # 第一次遇到 param, 初始化形状
-            if self.v[key] is None:
-                self.v[key] = np.zeros_like(param)
-
-            # v[key] = beta * v[key] + (1-beta)*grad^2
-            self.v[key] = self.beta * self.v[key] + (1.0 - self.beta) * (grad ** 2)
-
-            # param = param - lr*(grad / sqrt(v+eps))
-            new_value = param - self.learning_rate * grad / (np.sqrt(self.v[key]) + self.epsilon)
-            param.assign(new_value)
+    def apply_gradients(self, weights, grads):
+        for i, (weight, grad) in enumerate(zip(weights, grads)):
+            self.v[i] = self.beta * self.v[i] + (1-self.beta) * (grad) ** 2
+            weights[i] -= ((self.learning_rate) / ((self.v[i]) ** (1/2) + self.epsilon) * grad)
 
 
 class Adam:
@@ -52,7 +43,7 @@ class Adam:
         epsilon=1e-7
     """
     def __init__(
-        self, learning_rate=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-7, amsgrad=False
+        self, learning_rate, beta_1=0.9, beta_2=0.999, epsilon=1e-7, amsgrad=False
     ):
         self.learning_rate = learning_rate
         self.beta_1 = beta_1
@@ -62,26 +53,18 @@ class Adam:
 
         self.m = defaultdict(lambda: None)  # 一阶动量
         self.v = defaultdict(lambda: None)  # 二阶动量
+        self.m_hat = defaultdict(lambda: 0)     # Expected value of first moment vector
+        self.v_hat = defaultdict(lambda: 0)     # Expected value of second moment vector
         self.t = 0                           # 时间步
 
-    def apply_gradients(self, trainable_params, grads):
+    def apply_gradients(self, weights, grads):
         self.t += 1
-        for param, grad in zip(trainable_params, grads):
-            key = id(param)
-            # 若是第一次见到 param => 初始化
-            if self.m[key] is None:
-                self.m[key] = np.zeros_like(param)
-                self.v[key] = np.zeros_like(param)
+        # m represents momentum to propel gradient, v is like acceleration, adjusts learning rate
+        for i in range(len(weights)):
+            self.m[i] = (self.beta_1 * self.m[i]) + ((1-self.beta_1) * grads[i])
+            self.v[i] = (self.beta_2 * self.v[i]) + ((1-self.beta_2) * (grads[i]**2))
 
-            # m = beta1*m + (1-beta1)*grad
-            self.m[key] = self.beta_1 * self.m[key] + (1.0 - self.beta_1)*grad
-            # v = beta2*v + (1-beta2)*(grad^2)
-            self.v[key] = self.beta_2 * self.v[key] + (1.0 - self.beta_2)*(grad**2)
+            self.m_hat[i] = self.m[i] / (1-(self.beta_1 ** self.t))
+            self.v_hat[i] = self.v[i] / (1-(self.beta_2 ** self.t))
 
-            # 偏差修正
-            m_hat = self.m[key] / (1.0 - self.beta_1**self.t)
-            v_hat = self.v[key] / (1.0 - self.beta_2**self.t)
-
-            # param = param - lr*m_hat/(sqrt(v_hat)+eps)
-            new_value = param - self.learning_rate * m_hat / (np.sqrt(v_hat) + self.epsilon)
-            param.assign(new_value)
+            weights[i] -= self.learning_rate * (self.m_hat[i] / ((self.v_hat[i]) ** (1/2) + self.epsilon))
